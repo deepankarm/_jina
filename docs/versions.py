1b70badc3427c762c79f4625dcbe872477d4302d
Deepankar Mahapatro <deepankarmahapatro@gmail.com>
2021-11-29 21:55:06 +0530

    Update new-docs-build.yml

--- docs/versions.py
+++ docs/versions.py
@@ -7,16 +7,16 @@ import argparse
 import subprocess
 import urllib.request
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import List, Union
+from contextlib import ExitStack
 from tempfile import TemporaryDirectory
-from contextlib import ExitStack, contextmanager
 from packaging.version import InvalidVersion, Version
 
 from bs4 import BeautifulSoup
 
 
 def get_logger():
-    logFormatter = logging.Formatter(fmt=' %(name)s :: %(levelname)10s :: %(message)s')
+    logFormatter = logging.Formatter(fmt=' %(name)s:%(levelname)8s: %(message)s')
     logger = logging.getLogger('docs-versioning')
     logger.setLevel(logging.DEBUG)
     consoleHandler = logging.StreamHandler()
@@ -29,24 +29,15 @@ def get_logger():
 logger = get_logger()
 
 
-@contextmanager
-def change_cwd(path):
-    curdir = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(curdir)
-
-
 class CheckoutBranch(ExitStack):
     """
     Checkout a branch or tag in a temporary directory & cd into that.
     This assumes that the repo is already cloned.
     """
 
-    def __init__(self, branch_or_tag: str = 'gh-pages') -> None:
+    def __init__(self, repodir: str, branch_or_tag: str = 'gh-pages') -> None:
         super().__init__()
+        self.repodir = repodir
         self.branch_or_tag = branch_or_tag
 
     @staticmethod
@@ -57,17 +48,10 @@ class CheckoutBranch(ExitStack):
     def remove_worktree(tmpdir):
         subprocess.run(['git', 'worktree', 'remove', tmpdir])
 
-    @staticmethod
-    def commit():
-        commit_message = os.getenv(
-            'COMMIT_MESSAGE', 'chore(docs): update docs due to commit'
-        )
-        command = f'git status && git commit -m {commit_message} && git push --force origin main'
-        subprocess.run(command.split(' '))
-
     def __enter__(self):
         self._tmpdir = self.enter_context(TemporaryDirectory())
         self.curdir = os.getcwd()
+        os.chdir(self.repodir)
         logger.info(
             f'Creating a temp worktree for `{self.branch_or_tag}` in {self._tmpdir}'
         )
@@ -78,8 +62,50 @@ class CheckoutBranch(ExitStack):
 
     def __exit__(self, *args, **kwargs):
         os.chdir(self.curdir)
-        # TODO: self.remove_worktree(self._tmpdir)
+        self.remove_worktree(self._tmpdir)
         return super().__exit__(*args, **kwargs)
+
+
+class ClonedDocs(ExitStack):
+    def __init__(self, path: str) -> None:
+        super().__init__()
+        if not os.path.isdir(path):
+            raise FileNotFoundError(
+                'Invalid docs_path found. Please clone & pass `docs` repo path'
+            )
+        self.docsdir = path
+
+    def __enter__(self):
+        self.curdir = os.getcwd()
+        os.chdir(self.docsdir)
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        os.chdir(self.curdir)
+        return super().__exit__(*args, **kwargs)
+
+    @staticmethod
+    def commit():
+        commands = []
+        # if 'GITHUB_WORKFLOW' in os.environ:
+        #     print(f'\n\n\n\nHurray: {"GITHUB_TOKEN" in os.environ}\n\n\n')
+        #     commands.append(
+        #         f'git remote set-url origin https://deepankarm:{os.environ.get("GITHUB_TOKEN")}@github.com/jina-ai/staging-docs.git'
+        #     )
+        commit_message = os.getenv(
+            'COMMIT_MESSAGE', 'chore(docs): update docs due to commit'
+        )
+        commands.extend(
+            [
+                'git status',
+                'git config --local user.email "deepankar.mahapatro@jina.ai"',
+                'git config --local user.name "Deepankar Mahapatro"',
+                'git add -A',
+                f'git commit -m \"{commit_message}\"',
+                'git push --force origin main',
+            ]
+        )
+        [subprocess.run(command, shell=True) for command in commands]
 
 
 class VersionManagement:
@@ -88,8 +114,9 @@ class VersionManagement:
         self.docsdir = docsdir
         self._existing_versions = []
         self._non_existing_versions = []
-        self.last_n_versions = ['v2.5.0', 'v2.4.10', 'v2.4.9']  # 'v2.4.7', 'v2.4.6']
-        # self.last_n_versions = VersionManagement.get_last_n_versions(args.repo, args.num_releases)
+        self.last_n_versions = VersionManagement.get_last_n_versions(
+            args.repo, args.num_releases
+        )
         self.latest_version = self.last_n_versions[0]
 
     @property
@@ -122,14 +149,18 @@ class VersionManagement:
         ]
 
     @staticmethod
-    def build_local_only():
+    def build_local_only(version: str = ''):
         assert os.path.isfile(
             os.path.join(os.getcwd(), 'makedoc.sh')
         ), f'makedoc.sh file doesn\'t exist in current directory {os.getcwd()}'
 
         try:
-            f_content = '''import os
-os.environ['NUM_RELEASES'] = '1' # special case for 2.5.0, 2.4.10 & 2.4.9
+            if version in ['v2.5.0', 'v2.4.10', 'v2.4.9']:
+                f_content = '''import os
+os.system('sphinx-build . _build/dirhtml -b dirhtml') # special case for 2.5.0, 2.4.10 & 2.4.9
+'''
+            else:
+                f_content = '''import os
 os.system('bash makedoc.sh')
 '''
             with open(os.path.join(os.getcwd(), 'exec.py'), 'w') as f:
@@ -138,30 +169,33 @@ os.system('bash makedoc.sh')
         finally:
             os.remove(os.path.join(os.getcwd(), 'exec.py'))
 
-    def build_default_only(self, gh_page_dir):
+    def build_default_only(self, repodir, gh_page_dir):
         VersionManagement.build_versions(
             versions=self.args.default_branch_name,
-            gh_page_docs_dir=gh_page_dir,
+            repodir=repodir,
+            gh_page_dir=gh_page_dir,
             build_dir=self.args.build_dir,
         )
 
-    def build_latest_only(self, gh_page_dir):
+    def build_latest_only(self, repodir, gh_page_dir):
         VersionManagement.build_versions(
             versions=self.latest_version,
-            gh_page_docs_dir=gh_page_dir,
+            repodir=repodir,
+            gh_page_dir=gh_page_dir,
             build_dir=self.args.build_dir,
         )
 
-    def build_non_existing_versions(self, gh_page_dir):
+    def build_non_existing_versions(self, repodir, gh_page_dir):
         VersionManagement.build_versions(
             versions=self.non_existing_versions,
-            gh_page_docs_dir=gh_page_dir,
+            repodir=repodir,
+            gh_page_dir=gh_page_dir,
             build_dir=self.args.build_dir,
         )
 
     @staticmethod
     def build_versions(
-        versions: Union[str, List[str]], gh_page_docs_dir: str, build_dir: str
+        versions: Union[str, List[str]], repodir: str, gh_page_dir: str, build_dir: str
     ):
         # git cannot be run with multiple processes.
         if not isinstance(versions, List):
@@ -169,12 +203,12 @@ os.system('bash makedoc.sh')
 
         for version in versions:
             logger.info(f'Asked to rebuild version `{version}`, {os.getcwd()}')
-            with CheckoutBranch(branch_or_tag=version) as tag:
+            with CheckoutBranch(repodir=repodir, branch_or_tag=version) as tag:
                 # Inside {tmpdir}/docs where `version` is checked out.
-                VersionManagement.cleanup_version_directory(gh_page_docs_dir, version)
-                VersionManagement.build_local_only()
+                VersionManagement.cleanup_version_directory(gh_page_dir, version)
+                VersionManagement.build_local_only(version=version)
                 VersionManagement.recreate_version_directory(
-                    gh_page_docs_dir, tag, version, build_dir
+                    gh_page_dir, tag, version, build_dir
                 )
 
     @staticmethod
@@ -193,11 +227,25 @@ os.system('bash makedoc.sh')
         )
 
     def move_latest_version_to_root(self):
-        shutil.move(
-            src=os.path.join(self.docsdir, self.latest_version),
-            dst=self.docsdir,
-            copy_function=shutil.copytree,
-        )
+        version_dir = os.path.join(self.docsdir, self.latest_version)
+        if not os.path.isdir(version_dir):
+            logger.warning(
+                f'The latest version {self.latest_version} doesn\'t exist, please build first!'
+            )
+            return
+        for content in os.listdir(version_dir):
+            old_content_path = os.path.join(self.docsdir, content)
+            if os.path.isfile(old_content_path):
+                os.remove(old_content_path)
+            elif os.path.isdir(old_content_path):
+                shutil.rmtree(old_content_path)
+
+            shutil.move(
+                src=os.path.join(version_dir, content),
+                dst=self.docsdir,
+                copy_function=shutil.copytree,
+            )
+        shutil.rmtree(version_dir)
 
     def get_versions_status(self):
         """Get already existing versions in the gh-page branch"""
@@ -222,8 +270,12 @@ os.system('bash makedoc.sh')
                         self._existing_versions.append(dir)
                     except InvalidVersion:
                         continue
+
+        _relevant_versions = set(self.last_n_versions)
+        if not self.args.exclude_default_branch:
+            _relevant_versions.add(self.args.default_branch_name)
         self._non_existing_versions = list(
-            set(self.last_n_versions) - set(self._existing_versions)
+            _relevant_versions - set(self._existing_versions)
         )
 
     def update_dropdown_options(self, versions: Union[str, List[str]]):
@@ -261,7 +313,7 @@ os.system('bash makedoc.sh')
                             version_in_dropdown=version_in_dropdown,
                             version_in_dir=version_in_dir,
                         ):
-                            kwargs.update({'selected': True})
+                            kwargs.update({'selected': 'selected'})
                         option = html.new_tag(**kwargs)
                         option.string = VersionManagement.get_text_for_option(
                             version_in_dropdown=version_in_dropdown,
@@ -380,6 +432,17 @@ os.system('bash makedoc.sh')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Versions handler')
+
+    parser.add_argument('--repo', default='jina-ai/jina', help='Pass the repo name')
+    parser.add_argument(
+        '--repodir',
+        help='Pass the path to the already cloned repo (jina/finetuner)',
+    )
+    parser.add_argument(
+        '--docsdir',
+        help='Pass the path to the already cloned docs repo',
+    )
+
     parser.add_argument(
         '--local-only',
         help='Build local changed only, previous versions won\'t show up in docs',
@@ -403,6 +466,7 @@ if __name__ == '__main__':
         help='Pass the number of versions to be included in the dropdown',
         default=3,
     )
+
     parser.add_argument(
         '--default-branch-name',
         help='Pass the default branch name (master/main)',
@@ -420,13 +484,8 @@ if __name__ == '__main__':
         default='_build/dirhtml',
     )
     parser.add_argument(
-        '--docs-path',
-        help='Pass the docs directory path',
-        default=None,
-    )
-    parser.add_argument(
         '--commit',
-        help='True if changes to gh_pages branch needs to committed and pushed to remote',
+        help='True if changes to gh_pages branch needs to committed and pushed to the docs origin',
         default=False,
         action='store_true',
     )
@@ -437,36 +496,21 @@ if __name__ == '__main__':
         manager = VersionManagement(args, docsdir=os.getcwd())
         manager.build_local_only()
     else:
-        if args.docs_path:
-            if os.path.isfile(args.docs_path):
-                raise Exception(
-                    'Invalid docs_path found. Please clone & pass `docs` repo path'
-                )
-            gh_page = change_cwd(args.docs_path)
-        else:
-            gh_page = CheckoutBranch(
-                'gh-pages'
-            )  # TODO: Might change to cloning new repo
-
-        with gh_page:
-            # Inside `docs` repo
-            manager = VersionManagement(args, docsdir=gh_page.docsdir)
+        with ClonedDocs(args.docsdir) as gh_page:
+            # Inside already cloned `docs` repo
+            manager = VersionManagement(args, gh_page.docsdir)
 
             if args.default_only:
-                manager.build_default_only(gh_page_dir=gh_page.docsdir)
-                manager.update_dropdown_options(
-                    versions=manager.args.default_branch_name
-                )
+                manager.build_default_only(args.repodir, gh_page.docsdir)
+                manager.update_dropdown_options(manager.args.default_branch_name)
             elif args.latest_only:
-                manager.build_latest_only(gh_page_dir=gh_page.docsdir)
-                manager.update_dropdown_options(versions=manager.latest_version)
+                manager.build_latest_only(args.repodir, gh_page.docsdir)
+                manager.update_dropdown_options(manager.latest_version)
                 manager.move_latest_version_to_root()
             else:
                 manager.get_versions_status()
-                manager.build_non_existing_versions(gh_page_dir=gh_page.docsdir)
-                manager.update_dropdown_options(
-                    versions=manager.dropdown_versions_in_order
-                )
+                manager.build_non_existing_versions(args.repodir, gh_page.docsdir)
+                manager.update_dropdown_options(manager.dropdown_versions_in_order)
                 manager.move_latest_version_to_root()
 
             if args.commit:
